5a8605e30c20b61062d8c065dff2100748d7b925
Max <mdleypunskiy@yahoo.com>
2025-02-10 10:25:30 -0500

    Add initial project template

---
+++ App/Bookableu/Bookableu/BookableuApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookableuApp.swift
+//  Bookableu
+//
+//  Created by Maxim Leypunskiy on 10/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct BookableuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ App/Bookableu/Bookableu/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  Bookableu
+//
+//  Created by Maxim Leypunskiy on 10/02/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+            Text("2025 Bookableu")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
